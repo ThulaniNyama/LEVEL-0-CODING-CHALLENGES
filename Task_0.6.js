dd5791895d38423ed515c714375c734099b8f9bc
Thulani Nyama <nyamathulani@gmail.com>
2021-10-19 16:12:44 +0200

    second level 0 commit

---
+++ Task_0.6.js
@@ -0,0 +1,13 @@
+function maxNum()
+{
+  var max = 0;
+  if (num1 > num2 && num1 > num3) {
+    max = num1;
+  } else if (num2 > num3 && num2 > num1) {
+    max = num2;
+  }
+  else if (num3 > num1 && num3 > num2) {
+    max = num3;
+  }
+  return max;
+}
