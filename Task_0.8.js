b6cbfb72077a59423f3e516ebbec086dc0107868
Thulani Nyama <nyamathulani@gmail.com>
2021-10-20 00:35:08 +0200

    Task 0.8

--- Task_0.8.js
+++ Task_0.8.js
@@ -1,13 +1,28 @@
 function minutesToHours(time)
 {
-  let minutes = time % 60;
-  let hours = (time - minutes) / 60;
+  let minutes = 0;
+  let hours = 0;
   
-  if (hours > 1 && minutes > 1) {
-    console.log(hours + " hours, " + minutes + " minutes");
-  } else if (hours > 1 && minutes === 1) {
-    console.log(hours + " hours, " + minutes + " minute");
-  } else if (hours === 1 && minutes > 1) {
-    console.log(hours + " hour, " + minutes + " minutes");
-  }
+  if (time > 60) {
+    minutes = time % 60;
+    hours = (time - minutes) / 60;
+    if (hours > 1 && minutes > 1) {
+      console.log(hours + " hours, " + minutes + " minutes");
+    } else if (hours > 1 && minutes === 1) {
+      console.log(hours + " hours, " + minutes + " minute");
+    } else if (hours === 1 && minutes > 1) {
+      console.log(hours + " hour, " + minutes + " minutes");
+    } else if (hours === 1 && minutes === 1) {
+      console.log(hours + " hour, " + minutes + " minute");
+    }
+  } else if (time < 60){
+    minutes = time;
+    if (minutes > 1) {
+      console.log(minutes + " minutes");
+    } else if (minutes === 1) {
+      console.log(minutes + " minute");
+    } else if (time === 0) {
+      console.log(time);
+    }
+  } 
 }
